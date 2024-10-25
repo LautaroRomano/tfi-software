dbab54985ca2c6a204897e72c845011df1fecf8a
LautaroRomano <Lautarooyt837@gmail.com>
2024-10-25 17:39:21 -0300

    config shadcn

--- src/(components)/Sidebar.tsx
+++
@@ -1,9 +0,0 @@
-export default function Sidebar() {
-  return (
-    <div className="flex flex-col items-start justify-start w-64 h-screen shadow-lg text-black">
-      <h1>Medical clinic</h1>
-      <div className="font-bold text-center text-lg hover:opacity-8">Pacientes</div>
-      <div className="font-bold text-center text-lg hover:opacity-8">Usuarios</div>
-    </div>
-  );
-}
--- src/app/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Dashboard() {
-  return (
-    <div className="flex items-center justify-center w-screen h-screen">
-      <h1>Hola daashboard</h1>
-    </div>
-  );
-}
