c4a7ab23e7599d3819a78ae7df190644ce14db9b
LautaroRomano <71612453+LautaroRomano@users.noreply.github.com>
2024-11-07 12:35:47 -0300

    Paciente Dashboard

--- app/dashboard/pacientes/create-entity.tsx
+++ app/dashboard/pacientes/create-entity.tsx
@@ -1,22 +1,20 @@
-import { Patient } from "@/Models/dashboard/types";
+import { PacienteModel } from "@/Models/dashboard/types";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-type PropsType = {
-  data: null | Patient;
-  handleChangeData: (name: keyof Patient, value: string) => void;
-  save: (data: Patient) => void;
+interface PropsType {
+  data: null | PacienteModel;
+  handleChangeData: (name: keyof PacienteModel, value: string) => void;
+  save: (data: PacienteModel) => void;
   close: () => void;
-  labels: { [key in keyof Patient]?: string };
-};
+}
 
 export default function CreateEntity({
   data,
   handleChangeData,
   save,
   close,
-  labels,
 }: PropsType) {
   if (!data) return null;
 
@@ -27,26 +25,121 @@ export default function CreateEntity({
   return (
     <div className="flex absolute top-0 left-0 w-screen h-screen justify-center items-center z-50 bg-[#0005]">
       <div className="flex flex-col gap-4 p-6 border border-gray-300 rounded-lg shadow-md bg-white max-w-4xl">
-        <h2 className="text-lg font-semibold mb-4 dashboard-title">
-          Crear Nuevo Paciente
+        <h2 className="font-semibold mb-4 dashboard-title">
+          {data?.id_paciente && data?.id_paciente > 0
+            ? "Actualizar datos"
+            : "Crear Nuevo Paciente"}
         </h2>
-        <div className="grid grid-cols-2 gap-8 m-4">
-        {(Object.keys(labels) as (keyof Patient)[]).map((key) => (
-            <div key={key} className="">
-              <Label htmlFor={key} className="font-semibold text-md">
-                {labels[key] || key}
-              </Label>
-              <Input
-                className="bg-gray-200"
-                id={key}
-                value={data[key as keyof Patient] || ""}
-                onChange={(e) =>
-                  handleChangeData(key as keyof Patient, e.target.value)
-                }
-                placeholder={`Ingrese ${labels[key] || key}`}
-              />
-            </div>
-          ))}
+        <div className="grid grid-cols-2 gap-8 m-2">
+          <div className="">
+            <Label htmlFor={"nombre"} className="font-semibold text-md">
+              Nombre
+            </Label>
+            <Input
+              className="bg-gray-200"
+              id={"nombre"}
+              name="nombre"
+              value={data.nombre}
+              onChange={({ target }) =>
+                handleChangeData("nombre", target.value)
+              }
+              placeholder={`Ingrese el nombre`}
+            />
+          </div>
+          <div className="">
+            <Label htmlFor={"apellido"} className="font-semibold text-md">
+              Apellido
+            </Label>
+            <Input
+              className="bg-gray-200"
+              id={"apellido"}
+              name="apellido"
+              value={data.apellido}
+              onChange={({ target }) =>
+                handleChangeData("apellido", target.value)
+              }
+              placeholder={`Ingrese el apellido`}
+            />
+          </div>
+          <div className="">
+            <Label htmlFor={"direccion"} className="font-semibold text-md">
+              Direccion
+            </Label>
+            <Input
+              className="bg-gray-200"
+              id={"direccion"}
+              name="direccion"
+              value={data.direccion}
+              onChange={({ target }) =>
+                handleChangeData("direccion", target.value)
+              }
+              placeholder={`Ingrese el direccion`}
+            />
+          </div>
+          <div className="">
+            <Label htmlFor={"dni"} className="font-semibold text-md">
+              DNI
+            </Label>
+            <Input
+              className="bg-gray-200"
+              id={"dni"}
+              name="dni"
+              value={data.dni}
+              onChange={({ target }) => handleChangeData("dni", target.value)}
+              placeholder={`Ingrese el dni`}
+            />
+          </div>
+
+          <div className="">
+            <Label htmlFor={"email"} className="font-semibold text-md">
+              Email
+            </Label>
+            <Input
+              className="bg-gray-200"
+              id="email"
+              name="email"
+              value={data.email}
+              onChange={({ target }) => handleChangeData("email", target.value)}
+              placeholder="Ingrese el email"
+            />
+          </div>
+
+          <div className="">
+            <Label
+              htmlFor={"fechaNacimiento"}
+              className="font-semibold text-md"
+            >
+              Fecha de Nacimiento
+            </Label>
+            <Input
+              type="date"
+              className="bg-gray-200"
+              id="fechaNacimiento"
+              name="fechaNacimiento"
+              value={formatDateToString(data.fechaNacimiento)} // convierte Date a string para el input
+              onChange={({ target }) =>
+                handleChangeData("fechaNacimiento", target.value)
+              }
+              placeholder="Ingrese la fechaNacimiento"
+            />
+          </div>
+
+          <div className="">
+            <Label htmlFor={"telefono"} className="font-semibold text-md">
+              Telefono
+            </Label>
+            <Input
+              type="number"
+              className="bg-gray-200"
+              id="telefono"
+              name="telefono"
+              value={data.telefono}
+              onChange={({ target }) =>
+                handleChangeData("telefono", target.value)
+              }
+              placeholder="Ingrese el telefono"
+            />
+          </div>
         </div>
         <div className="flex justify-end gap-2 mt-4">
           <Button variant="outline" onClick={close}>
@@ -58,3 +151,12 @@ export default function CreateEntity({
     </div>
   );
 }
+
+// Función para convertir Date a string en formato YYYY-MM-DD
+const formatDateToString = (date: Date | null): string => {
+  if (!date) return "";
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // mes en 2 dígitos
+  const day = String(date.getDate()).padStart(2, "0"); // día en 2 dígitos
+  return `${year}-${month}-${day}`;
+};
--- app/dashboard/pacientes/page.tsx
+++ app/dashboard/pacientes/page.tsx
@@ -5,22 +5,37 @@ import { getPatients } from "./functions";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useEffect, useState } from "react";
-import { Patient } from "@/Models/dashboard/types";
+import { HistoriaClinicaModel, PacienteModel } from "@/Models/dashboard/types";
 import CreateEntity from "@/app/dashboard/pacientes/create-entity";
+import { useSearchParams } from "next/navigation";
 
-const createPatientData: Patient | null = {
-  id: "",
-  name: "",
+const historiaClinica: HistoriaClinicaModel = {
+  id_historia_clinica: 0,
+  diagnosticos: [],
+};
+
+const createPatientData: PacienteModel | null = {
+  id_paciente: 0,
+  nombre: "",
+  apellido: "",
+  direccion: "",
+  dni: "",
+  fechaNacimiento: new Date(),
   email: "",
-  phone: "",
-  cuil: "",
-  birthDay: "",
+  telefono: 0,
+  historiaClinica: historiaClinica,
 };
 
 export default function Home() {
-  const [data, setData] = useState<Patient[]>([]);
+  const [data, setData] = useState<PacienteModel[]>([]);
   const [searchTerm, setSearchTerm] = useState("");
-  const [createPatient, setCreatePatient] = useState<Patient | null>(null);
+  const [createPatient, setCreatePatient] = useState<PacienteModel | null>(
+    null
+  );
+
+  const searchParams = useSearchParams();
+  const editId = searchParams.get("edit");
+  const deleteId = searchParams.get("delete");
 
   const getData = async (search: string) => {
     const res = await getPatients(search);
@@ -30,6 +45,22 @@ export default function Home() {
   useEffect(() => {
     getData("");
   }, []);
+
+  useEffect(() => {
+    if (editId && editId.length > 0) {
+      const paciente = data.find((f) => f.id_paciente === parseInt(editId));
+      setCreatePatient(paciente || null);
+    }
+  }, [editId]);
+
+  useEffect(() => {
+    if (deleteId && deleteId.length > 0) {
+      const paciente = data.find((f) => f.id_paciente === parseInt(deleteId));
+      const res = window.confirm("Esta seguro que desea eliminar el paciente?");
+      if (res) console.log("🚀 Eliminar paciente", paciente);
+      else console.log("🚀 Cancelar eliminacio (salvao)", paciente);
+    }
+  }, [deleteId]);
 
   useEffect(() => {
     if (searchTerm.length === 0) getData("");
@@ -43,8 +74,15 @@ export default function Home() {
     setCreatePatient(createPatientData);
   };
 
-  const handleChangeData = (name: keyof Patient, value: string) => {
+  const handleChangeData = (name: keyof PacienteModel, value: string) => {
+    if (name === "fechaNacimiento") {
+      setCreatePatient((prev) =>
+        prev ? { ...prev, fechaNacimiento: new Date(value) } : null
+      );
+      return;
+    }
     setCreatePatient((prev) => (prev ? { ...prev, [name]: value } : null));
+    return;
   };
 
   return (
@@ -53,14 +91,7 @@ export default function Home() {
         data={createPatient}
         close={() => setCreatePatient(null)}
         handleChangeData={handleChangeData}
-        save={(data: Patient) => console.log("crear paciente", data)}
-        labels={{
-          name: "Nombre Completo",
-          email: "Correo Electrónico",
-          phone: "Teléfono",
-          cuil: "CUIL",
-          birthDay: "Fecha de nacimiento",
-        }}
+        save={(data: PacienteModel) => console.log("crear paciente", data)}
       />
       <div className="container mx-auto px-10 py-4 w-full">
         <div className="flex items-center justify-end gap-4 mb-4">
